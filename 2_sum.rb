1c4536870d100086244db938498312b6d1cbfaf3
Karthik Naidu <karthik.naidu@lookout.com>
2015-06-02 16:44:03 -0700

    simplified 2 sum solution

--- 2_sum.rb
+++ 2_sum.rb
@@ -1,32 +1,17 @@
 def find_pair(array, sum)
-  number_hash = convert_to_hash(array)
-  array.each do |e|
-    number_hash[e] -=1
-    if pair_exists?
-      puts "Pair found: #{e} and #{sum-e}"
-      break
-    end
-    number_hash[e] +=1
-  end
-
-  puts 'Pair not found'
-end
-
-def pair_exists?(e, sum)
-  number_hash.include?(sum - e) && number_hash[sum-e] > 0
-end
-
-def convert_to_hash(array)
-  number_hash = {}
-  array.each do |e|
-    if number_hash.include?(e)
-      number_hash[e] += 1
+  i = 0
+  pair_exists = {}
+  while i < array.size
+    if pair_exists.include?(array[i])
+      return true
     else
-      number_hash[e] = 1
+      pair_exists[sum - array[i]] = true
     end
+    i += 1
   end
 
-  number_hash
+  return false
 end
 
-find_pair([1,2,3,4,5], 5)
+
+puts find_pair([1,2,3,4,5], 3)
